af2c65876e0c63f359a2e255ac2f3bb2d3943e15
karthisb15@gmail.com <karthisb15@gmail.com>
2025-01-01 12:32:59 +0530

    Initial commit

---
+++ src/test/java/day6/Student.java
@@ -0,0 +1,43 @@
+package day2;
+
+public class Pojo_PostReuest {
+    String name;
+    String id;
+    int age;
+    String gender;
+
+
+
+    String courses[];
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String[] getCourses() {
+        return courses;
+    }
+    public void setCourses(String[] courses) {
+        this.courses = courses;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ src/test/java/day8/petstore/DeleteUser.java
@@ -0,0 +1,2 @@
+package day8.petstore;public class DeleteUser {
+}
